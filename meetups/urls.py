c410270ae8375036b873f77bbb331775584928f0
Joshua <davidjoshua026@gmail.com>
2022-02-02 13:37:43 +0100

    added app "meetups" to the project

---
+++ meetups/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# django looks through all the urls.py files looking for the variable "urlpatterns".
+# Therefore always name it properly.
+# Always add a "/" to the end of the url name because i
+urlpatterns = [
+    path('meetups/', views.index)
+]
