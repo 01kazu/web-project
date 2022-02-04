827524b7443dd12d31e6b65e0e0200762c934f64
01kazu <davidjoshua026@gmail.com>
2022-02-04 15:53:52 +0100

    Home page has been created

--- meetups/views.py
+++ meetups/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse('Hello World')
+
 def index(request):
-    return HttpResponse('Hello World')
+    meetups = [
+        {'title': 'A first meetup', 'location': 'Abuja', 'slug' : 'a-first-meetup'},
+        {'title': 'A Second meetup', 'location': 'Lagos', 'slug': 'a-second-meetup'},
+    ]
+    return render(request, 'meetups/index.html', {'meetups' : meetups, 'condition': False})
