7d04a6f7696d9fe89c4950dfc362af34e38e8de1
Joshua <davidjoshua026@gmail.com>
2022-02-09 15:29:05 +0100

    added Participant model and added attributes to Meetup model

--- meetups/models.py
+++ meetups/models.py
@@ -10,12 +10,22 @@ class Location(models.Model):
         return f'{self.name} - {self.address}'
 
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug  = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to = 'images')
     location = models.ForeignKey(Location, on_delete = models.SET_NULL, null=True)
+    participants = models.ManyToManyField(Participant, blank=True)
+    organizer_email = models.EmailField(blank=True)
+    date = models.DateField(blank=True)
 
     def __str__(self):
         return self.title
