3615e392322a7f3a203edb317b5d2a4393b89d8e
Joshua <davidjoshua026@gmail.com>
2022-02-09 15:43:48 +0100

    added the meetup_confirmation function or more code to meetup_detail

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Meetup
+from django.shortcuts import render, redirect
+
+from .forms import RegistrationForm
+from .models import Meetup, Participant
 
 # Create your views here.
 
@@ -23,12 +25,31 @@ def meetup_details(request, meetup_slug):
     #     }
     try:
         selected_meetup  = Meetup.objects.get(slug = meetup_slug)
+        if request.method == "POST":
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                participant = form.save()
+                selected_meetup.participants.add(participant)
+                return redirect('meetup-confirmation', meetup_slug=meetup_slug, confirm="success")
+        else:
+            form = RegistrationForm()
         return render(request, 'meetups/meetup-detail.html', {
             'meetup_detail': selected_meetup,
-            'meetup_found': True
+            'meetup_found': True,
+            'form' : form,
         })
     except Exception as exc:
         return render(request, 'meetups/meetup-detail.html', {
-            'meetup_found': False
+            'meetup_found': False,
         })
+
+
+def meetup_confirmation(request, meetup_slug, confirm):
+    meetup  = Meetup.objects.get(slug = meetup_slug)
+    webpage_info = {
+        'meetup': meetup,
+        'confirm': confirm,
+    }
+    return render(request, 'meetups/meetup-confirmation.html', webpage_info)
+
     
